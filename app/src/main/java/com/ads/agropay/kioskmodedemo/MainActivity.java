a75eae938041a241c4b05d9e263d88aea485c7bd
Masumba <masumbakayombo1@gmail.com>
2018-12-18 14:59:36 +0200

    V1.1

--- app/src/main/java/com/ads/agropay/kioskmodedemo/MainActivity.java
+++ app/src/main/java/com/ads/agropay/kioskmodedemo/MainActivity.java
@@ -1,20 +1,13 @@
 package com.ads.agropay.kioskmodedemo;
 
-import android.app.ActivityManager;
+
 import android.app.ActivityOptions;
 import android.app.admin.DevicePolicyManager;
-import android.app.admin.SystemUpdatePolicy;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.os.BatteryManager;
 import android.os.Build;
-import android.os.UserManager;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -24,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnLock;
 
     /**/
+    public int clickCount = 0;
     static final int RESULT_ENABLE = 1;
     DevicePolicyManager devicePolicyManager;
     ComponentName componentName;
@@ -50,20 +44,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 /**/
-                if (!kioskModeActive){
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        startLockTask();
-                        kioskModeActive = true;
-                    }
-                } else if (kioskModeActive){
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        stopLockTask();
-                        kioskModeActive = false;
-                    }
-                }
+                runKioskMode();
                 /**/
             }
         });
+    }
+
+    private void runKioskMode() {
+        clickCount = 4;
+
+        if (!kioskModeActive){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                startLockTask();
+                kioskModeActive = true;
+            }
+        } else if (kioskModeActive){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                stopLockTask();
+                kioskModeActive = false;
+            }
+        }
+    }
+
+    public void kioskMode(View view){
+        clickCount = clickCount+1;
+
+        Toast.makeText(this,"Count: "+clickCount,Toast.LENGTH_SHORT).show();
+
+        if (clickCount == 5){
+            runKioskMode();
+        }
     }
 
 }
